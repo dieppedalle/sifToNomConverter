29e2eb5fc436c8366a9d3386e31d00c55934854f
Gauthier Dieppedalle <gauthier.dieppedalle@gmail.com>
2016-12-22 22:15:26 -0800

    Added comments

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# main.py is used to convert .sif files to .nom
 import re
 import sys
 
@@ -6,14 +7,23 @@ listFaces = list()
 newFaces = list()
 
 def convertFileToString(filename):
+    """
+    Function is open input file and store it as a string
+    """
     with open('input/' + filename, 'r') as myfile:
-        data=myfile.read()#.replace('\n', '')
+        data=myfile.read()
     return data
 
 def tokenize(chars):
+    """
+    Convert file from a string to an array of tokens
+    """
     return chars.replace('(', ' ( ').replace(')', ' ) ').split()
 
 def parse(tokens):
+    """
+    Parses the file to remove parenthesis
+    """
     if (len(tokens)==0):
         raise SyntaxError("File is empty")
     currentToken = tokens.pop(0)
@@ -30,22 +40,26 @@ def parse(tokens):
         return currentToken
 
 def eval(x, outputFile, isSquare):
+    """
+    Writes the converted files to an output file
+    """
     if not isinstance(x, list):
-        #print(x)
         return x
     elif x[0] == "vertices":
         numberVertices = int(x[1])
         currentVertex = 0
-        #print numberVertices
+
+        # Converts vertices to .nom syntax
         for i in range(numberVertices):
             outputFile.write("point v" + str(i) + " (" + x[(2+i)][1] + " " + x[(2+i)][2] + " " + x[(2+i)][3] + ") endpoint\n")
         outputFile.write("\n")
+
     elif x[0] == "triangles":
         listFaces = []
         newFaces = []
         numberTriangles = int(x[1])
         currentTriangle = 0
-        #print numberTriangles
+
         meshName = "SIFmesh" + str(len(listMeshes))
         listMeshes.append(meshName)
 
@@ -54,6 +68,7 @@ def eval(x, outputFile, isSquare):
         for i in range(numberTriangles):
             listFaces.append(x[(2+i)][1:])
 
+        # Checks if we need to convert triangle faces to rectangles
         if isSquare == True:
             mergeFaces(listFaces, newFaces)
 
@@ -66,7 +81,7 @@ def eval(x, outputFile, isSquare):
                 outputFile.write(vertex)
             outputFile.write(") endface\n")
 
-
+        # Checks whether ot not there are any rectangle faces to add
         for i, element in enumerate(newFaces):
             outputFile.write("face f" + str(len(listFaces)+i) + " (")
 
@@ -78,41 +93,60 @@ def eval(x, outputFile, isSquare):
         outputFile.write("endmesh\n\n")
 
     else:
+        # Continue to process data
         proc = eval(x[0], outputFile, isSquare)
         args = [eval(arg, outputFile, isSquare) for arg in x[1:]]
 
 def createMeshes(outputFile):
+    """
+    Writes all the meshes created to the outputFile
+    """
     for i, mesh in enumerate(listMeshes):
         outputFile.write("instance  mesh" + str(i) + " " + mesh + " scale (1 1 1)  endinstance\n")
 
 def createOutputFile():
+    """
+    Create an output stream
+    """
     outputFile = open("output/" + outputName + ".nom", "w")
     outputFile.write("####  CONVERTED FROME A SIF  FILE  ####\n\n")
     return outputFile
 
 
 def removeComments(stringFile):
+    """
+    Removes all of the comments from the string
+    """
     stringFile=stringFile.replace('\n', ' ')
     return (re.sub("(\(\*).*?(\*\))", "", stringFile))
-    #print(stringFile)
 
 def mergeFaces(listFaces, newFaces):
-
-
+    """
+    Function used to merge triangles faces into rectangle faces
+    """
     while len(listFaces) != 0:
         currentList = listFaces.pop(0)
         for i, element in enumerate(listFaces):
-            #print(element)
             if len(list(set(currentList).intersection(element))) == 2:
                 newFaces.append(list(set(currentList + element)))
                 listFaces.pop(i)
                 break;
 
-
-#squarefaces=True
-
-
 def main():
+    """
+    main Function
+    """
+    # Reads the argument from the command
+    inputName = sys.argv[1]
+    outputName = sys.argv[2]
+
+    # Checks if need to convert triangle faces to rectangles
+    if len(sys.argv) > 3:
+        squarefaces = sys.argv[3]
+    else:
+        squarefaces = False
+
+    # Parsing the string
     stringFile = convertFileToString(inputName + '.sif')
     removeComments(stringFile)
     tokens = tokenize(stringFile)
@@ -120,10 +154,8 @@ def main():
 
     outputFile = createOutputFile()
 
+    # Convert .sif to .nom
     eval(arrayFile, outputFile, squarefaces)
     createMeshes(outputFile)
 
-inputName = sys.argv[1]
-outputName = sys.argv[2]
-squarefaces = sys.argv[3]
 main()
